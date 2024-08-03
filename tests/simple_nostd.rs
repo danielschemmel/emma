682dfe72c7245545e57d272c84536496be25cfeb
Daniel Schemmel <daniel@schemmel.net>
2024-08-03 17:41:52 +0100

    reenable test

--- tests/simple_nostd.rs
+++ tests/simple_nostd.rs
@@ -9,12 +9,12 @@ static EMMA: DefaultEmma = DefaultEmma::new();
 
 #[test]
 fn simple_vecs() {
-	//let mut v = alloc::vec![1, 2, 3];
-	//let target = v.capacity() + 20;
-	//while v.len() < target {
-	//	v.push(42);
-	//}
+	let mut v = alloc::vec![1, 2, 3];
+	let target = v.capacity() + 20;
+	while v.len() < target {
+		v.push(42);
+	}
 
-	//let or = v.iter().fold(0, |acc, x| acc | x);
-	//assert_eq!(or, 42 | 1 | 2 | 3);
+	let or = v.iter().fold(0, |acc, x| acc | x);
+	assert_eq!(or, 42 | 1 | 2 | 3);
 }
